af9f78f504553777f14f4944c33ef4694101198b
Maliksharjeel37 <sharjeeltariq37@gmail.com>
2023-09-03 11:40:01 +0500

    deploy

--- app/(components)/navber.js
+++ app/(components)/navber.js
@@ -6,25 +6,25 @@ function navber() {
       <nav className="justify-start">
         <ul className="flex , flex-werp justify-center  my-2">
           <h1 className=" fout-bold mr-96 font-bold text-3xl text-white">LOGO</h1>
-          <li className="mx-8 text-lg text-white hover:text-black">
+          <li className="mx-4 text-lg text-white hover:text-black">
             <Link href="/">HOME</Link>
           </li>
-          <li className="mx-8 text-lg text-white hover:text-stone-950">
+          <li className="mx-4 text-lg text-white hover:text-stone-950">
             <Link href={"/SERVICES"}>SERVICES</Link>
           </li>
-          <li className="mx-8 text-lg text-white hover:text-stone-950">
+          <li className="mx-4 text-lg text-white hover:text-stone-950">
             <Link href={"/ABOUT"}>ABOUT US</Link>
           </li>
-          <li className="mx-8 text-lg text-white hover:text-stone-950">
+          <li className="mx-4 text-lg text-white hover:text-stone-950">
             <Link href={"/PORTFOLIO"}>PORTFOLIO</Link>
           </li>
-          <li className="mx-8 text-lg text-white hover:text-stone-950">
+          <li className="mx-4 text-lg text-white hover:text-stone-950">
             <Link href={"/TEAM"}>TEAM</Link>
           </li>
-          <li className="mx-8 text-lg text-white hover:text-stone-950">
+          <li className="mx-4 text-lg text-white hover:text-stone-950">
             <Link href={"/PRICING"}>PRICING</Link>
           </li>
-          <li className="mx-8 text-lg text-white hover:text-black">
+          <li className="mx-4 text-lg text-white hover:text-black">
             <Link href={"/BLOG"}>BLOG</Link>
           </li>
           <Input />
